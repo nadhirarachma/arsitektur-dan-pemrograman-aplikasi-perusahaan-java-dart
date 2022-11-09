65dcf2375e1099360decb33487a5b30c09da7fd5
Alviona Retno Amalia <alviona.retno@ui.ac.id>
2022-11-09 11:48:13 +0700

    setup gradle

---
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/model/ResepModel.java
@@ -0,0 +1,38 @@
+package apap.ta.rumahsehat.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="resep")
+public class ResepModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "isDone", nullable = false)
+    private Boolean isDone;
+
+    @NotNull
+    @Column(nullable = false)
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime createdAt;
+}
