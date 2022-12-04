0a3cbe3efb3b49e882057ecde9533d6d8595f524
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2022-12-04 20:12:32 +0700

    passing username

--- rumahsehat_mobile/lib/page/homepage.dart
+++ rumahsehat_mobile/lib/page/homepage.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:rumahsehat_mobile/page/profile.dart';
 import 'package:rumahsehat_mobile/widget/drawer.dart';
 
 class Home extends StatefulWidget {
   static const route = "/home";
 
+  const Home({Key? key, required this.username}) : super(key: key);
+  final String username;
   @override
   _Homepage createState() => _Homepage();
 }
@@ -16,26 +19,28 @@ class _Homepage extends State<Home> {
         appBar: AppBar(
           title: const Text('Rumah Sehat'),
         ),
-        drawer: DrawerPage(),
+        drawer: DrawerPage(
+          username: widget.username,
+        ),
         body: SingleChildScrollView(
-
-            child: Column(
-                children: <Widget>[
-                GridView.count(
-                shrinkWrap: true,
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisCount: 3,
-                    scrollDirection: Axis.vertical,
-                    mainAxisSpacing: 5,
-                    crossAxisSpacing: 5,
-                    children: <Widget>[
+          child: Column(
+            children: <Widget>[
+              GridView.count(
+                  shrinkWrap: true,
+                  primary: false,
+                  padding: const EdgeInsets.all(20),
+                  crossAxisCount: 3,
+                  scrollDirection: Axis.vertical,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                  children: <Widget>[
                     GestureDetector(
                       onTap: () {
                         // Navigator.push(context,MaterialPageRoute(builder: (context) => AddAppointment()));
                       },
                       child: Card(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
                           elevation: 16,
                           margin: const EdgeInsets.all(9),
                           child: InkWell(
@@ -44,31 +49,30 @@ class _Homepage extends State<Home> {
                               },
                               splashColor: Colors.lightBlue,
                               child: Center(
-                                  child: Column(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: const <Widget>[
-                                          Icon(
-                                            Icons.add,
-                                            size: 25,
-                                            color: Colors.blueGrey,
-                                          ),
-                                          Text(
-                                            "Buat Appointment",
-                                            softWrap: true,
-                                            style: TextStyle(fontSize: 8),
-                                          ),
-                                      ],
-                                  ),
-                              )
-                          )
-                      ),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: const <Widget>[
+                                    Icon(
+                                      Icons.add,
+                                      size: 25,
+                                      color: Colors.blueGrey,
+                                    ),
+                                    Text(
+                                      "Buat Appointment",
+                                      softWrap: true,
+                                      style: TextStyle(fontSize: 8),
+                                    ),
+                                  ],
+                                ),
+                              ))),
                     ),
                     GestureDetector(
                       onTap: () {
                         // Navigator.push(context,MaterialPageRoute(builder: (context) => JadwalAppointment()));
                       },
                       child: Card(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
                           elevation: 16,
                           margin: const EdgeInsets.all(9),
                           child: InkWell(
@@ -92,21 +96,25 @@ class _Homepage extends State<Home> {
                                     ),
                                   ],
                                 ),
-                              )
-                          )
-                      ),
+                              ))),
                     ),
                     GestureDetector(
                       onTap: () {
                         // Navigator.push(context,MaterialPageRoute(builder: (context) => Profile()));
                       },
                       child: Card(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
                           elevation: 16,
                           margin: const EdgeInsets.all(9),
                           child: InkWell(
                               onTap: () {
-                                // Navigator.push(context,MaterialPageRoute(builder: (context) => Profile()));
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Profile(
+                                              username: widget.username,
+                                            )));
                               },
                               splashColor: Colors.lightBlue,
                               child: Center(
@@ -119,26 +127,32 @@ class _Homepage extends State<Home> {
                                       color: Colors.blueGrey,
                                     ),
                                     Text(
-                                      "Akun",
+                                      "Profile",
                                       softWrap: true,
                                       style: TextStyle(fontSize: 8),
                                     ),
                                   ],
                                 ),
-                              )
-                          )
-                      ),
+                              ))),
                     ),
                     GestureDetector(
-                      onTap: () {Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));},
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Home(
+                                      username: widget.username,
+                                    )));
+                      },
                       child: Card(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
                           elevation: 16,
                           margin: const EdgeInsets.all(9),
                           child: InkWell(
                               onTap: () {
                                 // Navigator.push(context,MaterialPageRoute(builder: (context) => DaftarTagihan()));
-                                },
+                              },
                               splashColor: Colors.lightBlue,
                               child: Center(
                                 child: Column(
@@ -156,16 +170,11 @@ class _Homepage extends State<Home> {
                                     ),
                                   ],
                                 ),
-                              )
-                          )
-                      ),
+                              ))),
                     ),
-                    ]
-
-                )
+                  ])
             ],
           ),
-        )
-    );
+        ));
   }
 }
