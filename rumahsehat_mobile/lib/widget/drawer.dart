31e8aed0ecf39e2b44957991800445b8d6eeb417
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2022-12-04 20:12:58 +0700

    passing username

--- rumahsehat_mobile/lib/widget/drawer.dart
+++ rumahsehat_mobile/lib/widget/drawer.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:rumahsehat_mobile/page/homepage.dart';
-
+import 'package:rumahsehat_mobile/page/profile.dart';
 
 class DrawerPage extends StatefulWidget {
-  const DrawerPage({Key? key}) : super(key: key);
+  const DrawerPage({Key? key, required this.username}) : super(key: key);
 
+  final String username;
   @override
   _DrawerState createState() => _DrawerState();
 }
@@ -18,86 +19,134 @@ class _DrawerState extends State<DrawerPage> {
           children: [
             GestureDetector(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Home(
+                              username: widget.username,
+                            )));
               },
               child: const ListTile(
                 leading: Icon(
                   Icons.home,
-                  size:30,
+                  size: 30,
                   color: Colors.black26,
-                ), title: Text(
-                  "Home", softWrap: true, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
-              ),
+                ),
+                title: Text(
+                  "Home",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
               ),
             ),
             GestureDetector(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Profile(
+                              username: widget.username,
+                            )));
               },
               child: const ListTile(
                 leading: Icon(
                   Icons.account_circle,
-                  size:30,
+                  size: 30,
                   color: Colors.black26,
-                ), title: Text(
-                "Akun", softWrap: true, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
-              ),
+                ),
+                title: Text(
+                  "Profile",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
               ),
             ),
             GestureDetector(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Home(
+                              username: widget.username,
+                            )));
               },
               child: const ListTile(
                 leading: Icon(
                   Icons.add,
-                  size:30,
+                  size: 30,
                   color: Colors.black26,
-                ), title: Text(
-                "Buat Appointment", softWrap: true, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
-              ),
+                ),
+                title: Text(
+                  "Buat Appointment",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
               ),
             ),
             GestureDetector(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Home(
+                              username: widget.username,
+                            )));
               },
               child: const ListTile(
                 leading: Icon(
                   Icons.today,
-                  size:30,
+                  size: 30,
                   color: Colors.black26,
-                ), title: Text(
-                "Jadwal Appointment", softWrap: true, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
-              ),
+                ),
+                title: Text(
+                  "Jadwal Appointment",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
               ),
             ),
             GestureDetector(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Home(
+                              username: widget.username,
+                            )));
               },
               child: const ListTile(
                 leading: Icon(
                   Icons.payment,
-                  size:30,
+                  size: 30,
                   color: Colors.black26,
-                ), title: Text(
-                "Tagihan", softWrap: true, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
-              ),
+                ),
+                title: Text(
+                  "Tagihan",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
               ),
             ),
             GestureDetector(
               onTap: () {
-                Navigator.push(context,MaterialPageRoute(builder: (context) => Home()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Home(
+                              username: widget.username,
+                            )));
               },
               child: const ListTile(
                 leading: Icon(
                   Icons.logout,
-                  size:30,
+                  size: 30,
                   color: Colors.black26,
-                ), title: Text(
-                "Logout", softWrap: true, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
-              ),
+                ),
+                title: Text(
+                  "Logout",
+                  softWrap: true,
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w200),
+                ),
               ),
             )
           ],
