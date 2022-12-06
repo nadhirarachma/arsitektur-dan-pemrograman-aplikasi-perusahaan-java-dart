44ae88fb4a1eec2a2439984cc30143b00fd8664e
Alviona Retno Amalia <alviona.retno@ui.ac.id>
2022-12-07 04:16:03 +0700

    menambahkan resep service

--- rumahsehat/src/main/java/apap/ta/rumahsehat/service/ResepService.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/service/ResepService.java
@@ -8,4 +8,7 @@ public interface ResepService {
     void addResep(ResepModel resep);
     //Method untuk mendapatkan daftar course yang telah tersimpan
     List<ResepModel> getListResep();
+    ResepModel getResepById(Long id);
+    ResepModel updateResep(ResepModel resep);
+    void deleteResep(ResepModel resep);
 }
