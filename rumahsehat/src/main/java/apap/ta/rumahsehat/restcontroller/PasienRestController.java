d456ff0f286fb21f95ecac5d21b2936efea5a6aa
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2022-12-08 00:12:35 +0700

    create lihat (list) tagihan BE

--- rumahsehat/src/main/java/apap/ta/rumahsehat/restcontroller/PasienRestController.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/restcontroller/PasienRestController.java
@@ -1,5 +1,7 @@
 package apap.ta.rumahsehat.restcontroller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 import javax.validation.Valid;
@@ -16,8 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import apap.ta.rumahsehat.model.AppointmentModel;
 import apap.ta.rumahsehat.model.PasienModel;
+import apap.ta.rumahsehat.model.TagihanModel;
 import apap.ta.rumahsehat.model.UserModel;
+import apap.ta.rumahsehat.payload.ListTagihanDTO;
 import apap.ta.rumahsehat.payload.PasienProfileDTO;
 import apap.ta.rumahsehat.payload.TopupDTO;
 import apap.ta.rumahsehat.service.PasienRestService;
@@ -60,7 +65,7 @@ public class PasienRestController {
 	// 	return userService.addUser(user);
 	// }
 
-	@GetMapping("/profile/{username}")
+	@GetMapping("/{username}/profile")
 	private PasienProfileDTO getPasienProfile(@PathVariable("username") String username){
 		try {
 			PasienModel pasien = pasienRestService.getPasienByUsername(username);
@@ -80,5 +85,19 @@ public class PasienRestController {
 		pasien.setSaldo(pasien.getSaldo() + topup.getJumlah());
 		pasienRestService.addPasien(pasien);
 		return pasien;
+	}
+
+	@GetMapping("/{username}/tagihan/viewall")
+	private ListTagihanDTO lihatDaftarTagihan(@PathVariable("username") String username){
+		ListTagihanDTO listTagihanDTO = new ListTagihanDTO();
+		PasienModel pasien = pasienRestService.getPasienByUsername(username);
+		List<TagihanModel> listTagihan = new ArrayList<>();
+		for (AppointmentModel a : pasien.getAppointment()) {
+			listTagihan.add(a.getTagihan());
+		}
+
+		listTagihanDTO.setNama(username);
+		listTagihanDTO.setListTagihan(listTagihan);
+		return listTagihanDTO;
 	}
 }
