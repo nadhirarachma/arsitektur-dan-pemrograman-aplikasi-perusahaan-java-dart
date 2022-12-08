1ee722a17cb8554014b15215c846b083050ede50
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2022-12-09 01:28:39 +0700

    Update AppointmentDb.java

--- rumahsehat/src/main/java/apap/ta/rumahsehat/repository/AppointmentDb.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/repository/AppointmentDb.java
@@ -2,10 +2,7 @@ package apap.ta.rumahsehat.repository;
 
 import apap.ta.rumahsehat.model.AppointmentModel;
 import apap.ta.rumahsehat.model.DokterModel;
-<<<<<<< rumahsehat/src/main/java/apap/ta/rumahsehat/repository/AppointmentDb.java
 import apap.ta.rumahsehat.model.PasienModel;
-=======
->>>>>>> rumahsehat/src/main/java/apap/ta/rumahsehat/repository/AppointmentDb.java
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
