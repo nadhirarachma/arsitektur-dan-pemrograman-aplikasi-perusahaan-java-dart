935416a6ea9c8edb15585fbe16805b8ee3490e7b
Alviona Retno Amalia <alviona.retno@ui.ac.id>
2022-12-09 23:43:15 +0700

    add dto for jumlah model

---
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/payload/JumlahDTO.java
@@ -0,0 +1,13 @@
+package apap.ta.rumahsehat.payload;
+
+import lombok.Data;
+
+@Data
+public class JumlahDTO {
+    private Integer kuantitas;
+    private Long id;
+    private String namaObat;
+}
+
+
+
