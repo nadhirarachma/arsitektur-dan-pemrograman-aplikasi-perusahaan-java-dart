dbe77ca7adff596becb2760b38550016d61bd1c5
Helga Syahda Elmira <helga.syahda@ui.ac.id>
2022-12-12 12:25:22 +0700

    Chart

--- rumahsehat/src/main/java/apap/ta/rumahsehat/service/ChartServiceImpl.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/service/ChartServiceImpl.java
@@ -4,16 +4,13 @@ import apap.ta.rumahsehat.model.AppointmentModel;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-<<<<<<< HEAD
+
 import java.time.LocalDateTime;
-=======
->>>>>>> 05a4af9c14b4d64f593bc9c19533b7b0acc07967
 import java.util.List;
 
 @Service
 public class ChartServiceImpl implements ChartService{
 
-<<<<<<< HEAD
 
     @Override
     public int[] getTagihan(List<AppointmentModel> appointmentModelList, LocalDateTime start, LocalDateTime end) {
@@ -27,15 +24,5 @@ public class ChartServiceImpl implements ChartService{
             tagihan[appointmentModel.getWaktuAwal().getDayOfMonth()-1]+=appointmentModel.getTagihan().getJumlahTagihan();
         }
         return tagihan;
-=======
-    @Override
-    public int[] getPendapatan(List<AppointmentModel> appointmentModelList) {
-        int[] pendapatan = new int[12];
-
-        for (AppointmentModel appointmentModel:appointmentModelList) {
-            pendapatan[appointmentModel.getWaktuAwal().getMonthValue()-1]+=appointmentModel.getDokter().getTarif();
-        }
-        return pendapatan;
->>>>>>> 05a4af9c14b4d64f593bc9c19533b7b0acc07967
     }
 }
