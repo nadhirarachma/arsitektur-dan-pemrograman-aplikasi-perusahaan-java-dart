df8b5c3a2bf16ecb8fbf6c56d1734b1ca54e1b36
Helga Syahda Elmira <helga.syahda@ui.ac.id>
2022-12-12 12:10:27 +0700

    Chart

--- rumahsehat/src/main/java/apap/ta/rumahsehat/controller/ChartController.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/controller/ChartController.java
@@ -23,7 +23,6 @@ public class ChartController {
     @RequestMapping("/chart")
     private String chart(Model model) {
         LocalDateTime dateTime=LocalDateTime.now();
-<<<<<<< HEAD
         LocalDateTime startDate = LocalDateTime.of(dateTime.getYear(), dateTime.getMonth(),1,0,0);
         LocalDateTime endDate = startDate.plusMonths(1).minusMinutes(1);
 
@@ -38,15 +37,6 @@ public class ChartController {
         model.addAttribute("tagihan",chartService.getTagihan(appointmentModelList,startDate,endDate));
         model.addAttribute("month",dateTime.getMonth());
         model.addAttribute("labels",days);
-=======
-        LocalDateTime startDate = LocalDateTime.of(dateTime.getYear(),1,1,0,0);
-        LocalDateTime endDate = LocalDateTime.of(dateTime.getYear(),12,1,0,0).plusMonths(1).minusMinutes(1);
-
-        List<AppointmentModel> appointmentModelList = appointmentService.getAppointmentAYear(startDate,endDate);
-
-        model.addAttribute("pendapatan",chartService.getPendapatan(appointmentModelList));
-        model.addAttribute("year",dateTime.getYear());
->>>>>>> 05a4af9c14b4d64f593bc9c19533b7b0acc07967
 
         return "chart";
     }
