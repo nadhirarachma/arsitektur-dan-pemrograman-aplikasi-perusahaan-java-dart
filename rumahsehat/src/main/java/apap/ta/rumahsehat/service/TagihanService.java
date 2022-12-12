f60c3fa24fef2bf389185cf1550106ec2cecfd7d
Al Ghifari Enerza Sentanu <al.ghifari01@ui.ac.id>
2022-12-12 11:09:31 +0700

    update fitur 16 BE

--- rumahsehat/src/main/java/apap/ta/rumahsehat/service/TagihanService.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/service/TagihanService.java
@@ -1,7 +1,11 @@
 package apap.ta.rumahsehat.service;
 
+import java.util.List;
+
+import apap.ta.rumahsehat.model.PasienModel;
 import apap.ta.rumahsehat.model.TagihanModel;
 
 public interface TagihanService {
     TagihanModel addTagihan(TagihanModel tagihan);
+    List<TagihanModel> getListTagihan(PasienModel pasien);
 }
