ac13398d891c1f3a3b8bb6c8397d662c55d45969
NADHIRA RACHMA SALSABILA ANANDRA <nadhira.rachma@ui.ac.id>
2022-12-23 09:54:58 +0700

    Clean Code Sonarqube

--- rumahsehat/src/main/java/apap/ta/rumahsehat/controller/PageController.java
+++ rumahsehat/src/main/java/apap/ta/rumahsehat/controller/PageController.java
@@ -15,10 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< rumahsehat/src/main/java/apap/ta/rumahsehat/controller/PageController.java
-=======
-import org.springframework.web.bind.annotation.PostMapping;
->>>>>>> rumahsehat/src/main/java/apap/ta/rumahsehat/controller/PageController.java
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.ModelAndView;
@@ -45,11 +41,7 @@ public class PageController {
         return "home";
     }
 
-<<<<<<< rumahsehat/src/main/java/apap/ta/rumahsehat/controller/PageController.java
     @GetMapping("/login")
-=======
-    @PostMapping("/login")
->>>>>>> rumahsehat/src/main/java/apap/ta/rumahsehat/controller/PageController.java
     public String login() {
        return "login";
     }
